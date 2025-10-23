1e89c36cecfc3b74b38ffb9df060ddc727a161e7
CiroManuelPenna <ciromanuel.penna001@studenti.uniparthenope.it>
2025-10-23 16:32:27 +0200

    Printing API response

--- app.py
+++ app.py
@@ -1,61 +1,36 @@
 #example script
-
+from flask import Flask, render_template, url_for, request, redirect
 import requests
 import os
 from dotenv import load_dotenv
 
+titles = []
+app = Flask(__name__)
 
-load_dotenv()  # reads .env file
-API_KEY = os.getenv("NEWS_API_KEY")
-BASE_URL = "https://newsapi.org/v2"
-HEADERS = {"X-Api-Key": API_KEY}
+@app.route("/")
+def index():
+    return render_template('index.html', titoli=titles)
 
-params = {
-    "q": "intelligenza artificiale",  # keyword(s)
-    "language": "it",                 # Italian
-    "from": "2025-10-01",
-    "to": "2025-10-21",
-    "sortBy": "publishedAt",          # relevancy | popularity | publishedAt
-    "pageSize": 50,                   # up to 100
-    "page": 1
-}
+if __name__ == "__main__":
+    load_dotenv()  # reads .env file
+    API_KEY = os.getenv("NEWS_API_KEY")
+    BASE_URL = "https://newsapi.org/v2"
+    HEADERS = {"X-Api-Key": API_KEY}
 
-response = requests.get(f"{BASE_URL}/everything", headers=HEADERS, params=params)
-data = response.json()
+    params = {
+        "q": "intelligenza artificiale",  # keyword(s)
+        "language": "it",                 # Italian
+        "from": "2025-10-01",
+        "to": "2025-10-21",
+        "sortBy": "publishedAt",          # relevancy | popularity | publishedAt
+        "pageSize": 50,                   # up to 100
+        "page": 1
+    }
 
-for article in data.get("articles", []):
-    print(article["title"])
+    response = requests.get(f"{BASE_URL}/everything", headers=HEADERS, params=params)
+    data = response.json()
 
-print("")
-print("")
-print("")
-print("")
+    for article in data.get("articles", []):
+        titles.append(article["title"])
 
-print(data.get("articles", [])[1]["content"])
-
-"""
-Response structure:
-{
-  "status": "ok",
-  "totalResults": <int>,
-  "articles": [ … ]
-}
-
-Each article has fields:
-
-source: object with id, name 
-
-author: string 
-
-title: string 
-
-description: string 
-
-url: string 
-
-urlToImage: string 
-
-publishedAt: string datetime in UTC 
-
-content: string (truncated to ~200 chars)
-"""
+    app.run(debug=True)
