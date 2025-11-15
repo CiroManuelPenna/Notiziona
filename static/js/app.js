f2c46d1b32d82d1a90d70eec7feaa634c17e57ed
CiroManuelPenna <ciromanuel.penna001@studenti.uniparthenope.it>
2025-11-15 16:29:48 +0100

    localStorage now stores not only the url of favorite articles, but also image and title

--- static/js/app.js
+++ static/js/app.js
@@ -1,25 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   const FAVORITES_KEY = "favorites";
-  const favorites = new Set(JSON.parse(localStorage.getItem(FAVORITES_KEY)) || []);
+  const favorites = new Map(JSON.parse(localStorage.getItem(FAVORITES_KEY)) || []);
 
   document.querySelectorAll(".fav_button").forEach(button => {
     const url = button.dataset.url;
+    const image = button.dataset.image;
+    const title = button.dataset.title;
     if (favorites.has(url)) {
         button.classList.add("active");
-        button.innerHTML = "In your favourites"
+        button.innerHTML = "In your favorites";
     }
 
     button.addEventListener("click", (event) => {
         event.stopPropagation(); // prevents click from propagating to parent elements
-        event.preventDefault();  // prevents the <a> link from triggering when click the "Add to Favorites" button
+        event.preventDefault();  // prevents the <a> link from triggering when click the "Add to favorites" button
         if (favorites.has(url)) {
             favorites.delete(url);
             button.classList.remove("active");
-            button.innerHTML = "★ Add to Favorites";
+            button.innerHTML = "★ Add to favorites";
         } else {
-            favorites.add(url);
+            favorites.set(url, {url, image, title});
             button.classList.add("active");
-            button.innerHTML = "In your favourites"
+            button.innerHTML = "In your favorites";
         }
         localStorage.setItem(FAVORITES_KEY, JSON.stringify([...favorites]));
     });
